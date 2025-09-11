026a5845d8c03c9065e7c7faa1377ca838bc808e
SanyaSho <sanyasho@worldspawn.party>
2025-09-11 15:44:23 +0300

    root: Add a switch between OSes in comp_proto.sh

--- comp_proto.sh
+++ comp_proto.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-$PWD/protobuf-2.6.1/bin/win64/2015/staticcrt/release/protoc.exe --proto_path=$PWD --python_out=$PWD --proto_path=$PWD/protobuf-2.6.1/src $PWD/gcsdk.proto
+
+case "$OSTYPE" in
+	msys*) 		$PWD/protobuf-2.6.1/bin/win64/2015/staticcrt/release/protoc.exe --proto_path=$PWD --python_out=$PWD --proto_path=$PWD/protobuf-2.6.1/src $PWD/gcsdk.proto ;;
+	linux*) 	$PWD/protobuf-2.6.1/bin/linux32/protoc --proto_path=$PWD --python_out=$PWD --proto_path=$PWD/protobuf-2.6.1/src $PWD/gcsdk.proto ;;
+esac
